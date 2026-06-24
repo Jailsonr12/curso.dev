17a4bc43bb84892c2b4460961f6c0e13f30fe09b
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2026-06-23 21:12:40 -0300

    fix: update database status endpoint to return dynamic version and connection details

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,20 +1,38 @@
-import database from "infra/database.js";
+import database from "/infra/database.js";
 
-async function status(req, res) {
+async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query("SELECT version();");
+  const databaseVerionResult = await database.query("SHOW server_version");
+  const databaseVersionvalue = databaseVerionResult.rows[0].server_version
+    .trim()
+    .split(" ")[0];
 
-  console.log("Database version result:", databaseVersionResult);
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionResultValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-  res.status(200).json({
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsResultValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  return response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        version: "16.0",
+        version: databaseVersionvalue,
+        max_connections: parseInt(databaseMaxConnectionResultValue),
+        opened_connections: databaseOpenedConnectionsResultValue,
       },
     },
   });
 }
-
 export default status;
