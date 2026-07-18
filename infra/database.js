9d021f08d3948ed9a44f2e9b1fc95125cd26f579
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2026-07-18 15:39:32 -0300

    refactor(database): improve error handling and SSL configuration
    - Introduced ServiceError for better error management in database queries.
    - Updated query function to handle errors more gracefully.
    - Ensured client connection is closed properly using optional chaining.
    - Cleaned up SSL configuration logic.
    
    chore(package): update dependencies and add eslint plugins
    
    - Updated eslint and eslint-config-next to latest versions.
    - Added eslint-config-prettier and eslint-plugin-jest for improved linting and testing.
    - Adjusted package-lock.json to reflect dependency changes.
    
    refactor(orchestrator): simplify export structure
    
    - Changed export of waitForAllServices to a direct export for cleaner code.

--- infra/database.js
+++ infra/database.js
@@ -1,29 +1,30 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
-  const shouldUseSsl = !["localhost", "127.0.0.1"].includes(
-    process.env.POSTGRES_HOST,
-  );
   let client;
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
 async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
 
@@ -31,10 +32,12 @@ async function getNewClient() {
   return client;
 }
 
-export default {
+const database = {
   query,
   getNewClient,
 };
+
+export default database;
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
@@ -42,5 +45,6 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
+
   return process.env.NODE_ENV === "production" ? true : false;
 }
